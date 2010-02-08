aa952292ee2499c7c5edb3a4ff1e9435057bd07e
Stephen Evanchik <evanchsa@gmail.com>
2010-02-07 22:54:45 -0500

    Adding Equinox Application definition for Apache Felix Karaf

---
+++ plugins/info.evanchik.karaf.app/src/main/java/info/evanchik/karaf/app/KarafApplication.java
@@ -0,0 +1,35 @@
+/**
+ * Copyright (c) 2010 Stephen Evanchik
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  Stephen Evanchik - initial implementation
+ */
+package info.evanchik.karaf.app;
+
+import org.eclipse.equinox.app.IApplication;
+import org.eclipse.equinox.app.IApplicationContext;
+
+/**
+ * @author Stephen Evanchik (evanchsa@gmail.com)
+ *
+ */
+public class KarafApplication implements IApplication {
+
+
+    @Override
+    public Object start(IApplicationContext context) throws Exception {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void stop() {
+        // TODO Auto-generated method stub
+
+    }
+
+}

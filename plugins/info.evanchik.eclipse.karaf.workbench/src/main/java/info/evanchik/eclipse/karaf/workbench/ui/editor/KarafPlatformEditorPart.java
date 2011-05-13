d1032bd5f82266994252f10137802a10c32cc2e9
Stephen Evanchik <evanchsa@gmail.com>
2011-05-12 23:27:19 -0400

    Refactoring editor to be a multi-page form editor

--- plugins/info.evanchik.eclipse.karaf.workbench/src/main/java/info/evanchik/eclipse/karaf/workbench/ui/editor/KarafPlatformEditorPart.java
+++ plugins/info.evanchik.eclipse.karaf.workbench/src/main/java/info/evanchik/eclipse/karaf/workbench/ui/editor/KarafPlatformEditorPart.java
@@ -14,115 +14,22 @@ import info.evanchik.eclipse.karaf.core.KarafPlatformDetails;
 import info.evanchik.eclipse.karaf.core.KarafPlatformModel;
 
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.jface.dialogs.Dialog;
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorSite;
 import org.eclipse.ui.PartInitException;
-import org.eclipse.ui.forms.events.ExpansionAdapter;
-import org.eclipse.ui.forms.events.ExpansionEvent;
-import org.eclipse.ui.forms.widgets.FormToolkit;
-import org.eclipse.ui.forms.widgets.ScrolledForm;
-import org.eclipse.ui.forms.widgets.Section;
-import org.eclipse.ui.part.EditorPart;
+import org.eclipse.ui.forms.editor.FormEditor;
 
 /**
  * @author Stephen Evanchik (evanchsa@gmail.com)
  *
  */
-public class KarafPlatformEditorPart extends EditorPart {
+public class KarafPlatformEditorPart extends FormEditor {
 
     public static final String ID = "info.evanchik.eclipse.karaf.ui.editors.KarafPlatformEditor";
-
-    private ScrolledForm form;
-
-    private FormToolkit formToolkit;
-
-    protected Text installDir;
-
-    protected Text platformDescription;
-
-    protected Text platformName;
-
-    protected Text platformVersion;
 
     private KarafPlatformModel karafPlatform;
 
     private KarafPlatformDetails platformDetails;
-
-    @Override
-    public void createPartControl(final Composite parent) {
-        formToolkit = new FormToolkit(parent.getDisplay());
-        formToolkit.setBorderStyle(SWT.BORDER);
-
-        form = formToolkit.createScrolledForm(parent);
-
-        final GridLayout layout = new GridLayout(2, false);
-
-        GridData data = new GridData(GridData.FILL_BOTH);
-        data.horizontalSpan = 2;
-
-        form.getBody().setLayout(layout);
-        form.getBody().setLayoutData(data);
-
-        final Section section = formToolkit.createSection(
-                form.getBody(),
-                  Section.DESCRIPTION
-                | Section.TITLE_BAR
-                | Section.TWISTIE
-                | Section.EXPANDED);
-
-        section.addExpansionListener(new ExpansionAdapter() {
-            @Override
-            public void expansionStateChanged(final ExpansionEvent e) {
-                form.reflow(true);
-            }
-        });
-
-        section.setText("Karaf Platform Overview");
-        section.setDescription("This is the description that goes below the title");
-
-        final Composite sectionClient = formToolkit.createComposite(section);
-        sectionClient.setLayout(new GridLayout(2, false));
-        sectionClient.setLayoutData(data);
-
-        section.setClient(sectionClient);
-
-        Label l = formToolkit.createLabel(sectionClient, "Name");
-        Dialog.applyDialogFont(l);
-
-        platformName = formToolkit.createText(sectionClient, platformDetails.getName(), SWT.BORDER);
-        data = new GridData(GridData.FILL_HORIZONTAL);
-        platformName.setLayoutData(data);
-        platformName.setEnabled(false);
-
-        l = formToolkit.createLabel(sectionClient, "Version");
-        Dialog.applyDialogFont(l);
-
-        platformVersion = formToolkit.createText(sectionClient, platformDetails.getVersion(), SWT.BORDER);
-        data = new GridData(GridData.FILL_HORIZONTAL);
-        platformVersion.setLayoutData(data);
-        platformVersion.setEnabled(false);
-
-        l = formToolkit.createLabel(sectionClient, "Description");
-        Dialog.applyDialogFont(l);
-
-        platformDescription = formToolkit.createText(sectionClient, platformDetails.getDescription(), SWT.BORDER);
-        data = new GridData(GridData.FILL_HORIZONTAL);
-        platformDescription.setLayoutData(data);
-        platformDescription.setEnabled(false);
-    }
-
-    @Override
-    public void dispose() {
-        formToolkit.dispose();
-        super.dispose();
-    }
 
     @Override
     public void doSave(final IProgressMonitor monitor) {
@@ -132,10 +39,17 @@ public class KarafPlatformEditorPart extends EditorPart {
     public void doSaveAs() {
     }
 
+    public KarafPlatformModel getKarafPlatform() {
+        return karafPlatform;
+    }
+
+    public KarafPlatformDetails getPlatformDetails() {
+        return platformDetails;
+    }
+
     @Override
     public void init(final IEditorSite site, final IEditorInput input) throws PartInitException {
-        setSite(site);
-        setInput(input);
+        super.init(site, input);
 
         @SuppressWarnings("unchecked")
         final EditableObject<KarafPlatformModel> accountEditableObject =
@@ -149,17 +63,16 @@ public class KarafPlatformEditorPart extends EditorPart {
     }
 
     @Override
-    public boolean isDirty() {
-        return false;
-    }
-
-    @Override
     public boolean isSaveAsAllowed() {
         return false;
     }
 
     @Override
-    public void setFocus() {
-        form.setFocus();
+    protected void addPages() {
+        try {
+            addPage(new KarafPlatformGeneralFormPage(this));
+        } catch (final PartInitException e) {
+            // TODO: Handle PartInitException
+        }
     }
 }
---
+++ plugins/info.evanchik.eclipse.karaf.workbench/src/main/java/info/evanchik/eclipse/karaf/workbench/ui/editor/KarafPlatformGeneralFormPage.java
@@ -0,0 +1,112 @@
+/**
+ * Copyright (c) 2011 Stephen Evanchik
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  Stephen Evanchik - initial implementation
+ */
+package info.evanchik.eclipse.karaf.workbench.ui.editor;
+
+import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.ui.forms.IManagedForm;
+import org.eclipse.ui.forms.editor.FormPage;
+import org.eclipse.ui.forms.events.ExpansionAdapter;
+import org.eclipse.ui.forms.events.ExpansionEvent;
+import org.eclipse.ui.forms.widgets.Section;
+
+/**
+ * @author Stephen Evanchik (evanchsa@gmail.com)
+ *
+ */
+public class KarafPlatformGeneralFormPage extends FormPage {
+
+    public static final String ID = "info.evanchik.eclipse.karaf.editors.page.General";
+
+    private static final String TITLE = "General";
+
+    protected Text installDir;
+
+    protected Text platformDescription;
+
+    protected Text platformName;
+
+    protected Text platformVersion;
+
+    private final KarafPlatformEditorPart editor;
+
+    public KarafPlatformGeneralFormPage(final KarafPlatformEditorPart editor) {
+        super(editor, ID, TITLE);
+
+        this.editor = editor;
+    }
+
+    @Override
+    protected void createFormContent(final IManagedForm managedForm) {
+        final GridLayout layout = new GridLayout(2, false);
+
+        GridData data = new GridData(GridData.FILL_BOTH);
+        data.horizontalSpan = 2;
+
+        managedForm.getForm().getBody().setLayout(layout);
+        managedForm.getForm().getBody().setLayoutData(data);
+
+        final Section section = managedForm.getToolkit().createSection(
+                managedForm.getForm().getBody(),
+                  Section.DESCRIPTION
+                | Section.TITLE_BAR
+                | Section.TWISTIE
+                | Section.EXPANDED);
+
+        section.addExpansionListener(new ExpansionAdapter() {
+            @Override
+            public void expansionStateChanged(final ExpansionEvent e) {
+                managedForm.reflow(true);
+            }
+        });
+
+        section.setText("Karaf Platform Overview");
+        section.setDescription("This is the description that goes below the title");
+
+        final Composite sectionClient = managedForm.getToolkit().createComposite(section);
+        sectionClient.setLayout(new GridLayout(2, false));
+        sectionClient.setLayoutData(data);
+
+        section.setClient(sectionClient);
+
+        Label l = managedForm.getToolkit().createLabel(sectionClient, "Name");
+        Dialog.applyDialogFont(l);
+
+        final String name = editor.getPlatformDetails().getName();
+        platformName = managedForm.getToolkit().createText(sectionClient, name, SWT.BORDER);
+        data = new GridData(GridData.FILL_HORIZONTAL);
+        platformName.setLayoutData(data);
+        platformName.setEnabled(false);
+
+        l = managedForm.getToolkit().createLabel(sectionClient, "Version");
+        Dialog.applyDialogFont(l);
+
+        final String version = editor.getPlatformDetails().getVersion();
+        platformVersion = managedForm.getToolkit().createText(sectionClient, version, SWT.BORDER);
+        data = new GridData(GridData.FILL_HORIZONTAL);
+        platformVersion.setLayoutData(data);
+        platformVersion.setEnabled(false);
+
+        l = managedForm.getToolkit().createLabel(sectionClient, "Description");
+        Dialog.applyDialogFont(l);
+
+        final String description = editor.getPlatformDetails().getDescription();
+        platformDescription = managedForm.getToolkit().createText(sectionClient, description, SWT.BORDER);
+        data = new GridData(GridData.FILL_HORIZONTAL);
+        platformDescription.setLayoutData(data);
+        platformDescription.setEnabled(false);
+    }
+}

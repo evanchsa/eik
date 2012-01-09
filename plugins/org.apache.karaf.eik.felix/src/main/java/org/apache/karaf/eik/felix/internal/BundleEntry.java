67a064f4c719bcd4052ceebf342792fba86cac84
jbonofre <jbonofre@13f79535-47bb-0310-9956-ffa450edef68>
2012-01-09 09:26:12 +0000

    Refactore of Eclipse felix wrapper
    git-svn-id: https://svn.apache.org/repos/asf/karaf/eik/trunk@1229069 13f79535-47bb-0310-9956-ffa450edef68

--- plugins/org.apache.karaf.eik.felix/src/main/java/org/apache/karaf/eik/felix/internal/BundleEntry.java
+++ plugins/org.apache.karaf.eik.felix/src/main/java/org/apache/karaf/eik/felix/internal/BundleEntry.java
@@ -1,30 +1,31 @@
-/**
- * Copyright (c) 2009 Stephen Evanchik
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
  *
- * Contributors:
- *  Stephen Evanchik - initial implementation
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
-package info.evanchik.eclipse.felix.internal;
+package org.apache.karaf.eik.felix.internal;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 
-/**
- * @author Stephen Evanchik (evanchsa@gmail.com)
- *
- */
 public class BundleEntry {
 
     /**
      * A builder to help facilitate the construction of {@link BundleEntry}
-     * objects
-     *
-     * @author Stephen Evanchik (evanchsa@gmail.com)
-     *
+     * objects.
      */
     public static class Builder {
 
@@ -140,9 +141,6 @@ public class BundleEntry {
     /**
      * A simple class to distinguish between simple bundle entries and URL
      * bundle entries.
-     *
-     * @author Stephen Evanchik (evanchsa@gmail.com)
-     *
      */
     private static class UrlBundleEntry extends BundleEntry {
 
@@ -227,4 +225,5 @@ public class BundleEntry {
 
         return sb.toString();
     }
+
 }

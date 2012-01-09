49be6b23761049d1018a2f113bb2f9e21801a4dc
Jean-Baptiste Onofre <jbonofre@apache.org>
2012-01-09 10:17:36 +0000

    Refactoring of EIK WTP UI
    git-svn-id: https://svn.apache.org/repos/asf/karaf/eik/trunk@1229081 13f79535-47bb-0310-9956-ffa450edef68

--- plugins/org.apache.karaf.eik.wtp.ui/src/main/java/org/apache/karaf/eik/wtp/ui/launcher/KarafLauncherTabGroup.java
+++ plugins/org.apache.karaf.eik.wtp.ui/src/main/java/org/apache/karaf/eik/wtp/ui/launcher/KarafLauncherTabGroup.java
@@ -1,14 +1,22 @@
-/**
- * Copyright (c) 2009 Stephen Evanchik
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
  *
- * Contributors:
- *  Stephen Evanchik - initial implementation
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
-package info.evanchik.eclipse.karaf.wtp.ui.launcher;
+package org.apache.karaf.eik.wtp.ui.launcher;
 
 import org.apache.karaf.eik.ui.KarafConfigurationTab;
 import org.apache.karaf.eik.ui.KarafLaunchConfigurationInitializer;
@@ -25,10 +33,6 @@ import org.eclipse.pde.ui.launcher.OSGiSettingsTab;
 import org.eclipse.pde.ui.launcher.TracingTab;
 import org.eclipse.wst.server.ui.ServerLaunchConfigurationTab;
 
-/**
- * @author Stephen Evanchik (evanchsa@gmail.com)
- *
- */
 public class KarafLauncherTabGroup extends AbstractLaunchConfigurationTabGroup {
 
     /**
@@ -39,7 +43,7 @@ public class KarafLauncherTabGroup extends AbstractLaunchConfigurationTabGroup {
     @Override
     public void createTabs(final ILaunchConfigurationDialog dialog, final String mode) {
         final ILaunchConfigurationTab[] tabs = new ILaunchConfigurationTab[] {
-                new ServerLaunchConfigurationTab(new String[] { "info.evanchik.eclipse.karaf.server" }),  //$NON-NLS-1$
+                new ServerLaunchConfigurationTab(new String[] { "org.apache.karaf.eik.server" }),
                 new JavaArgumentsTab(),
                 new OSGiSettingsTab(),
                 new TracingTab(),
@@ -62,4 +66,5 @@ public class KarafLauncherTabGroup extends AbstractLaunchConfigurationTabGroup {
     public void setDefaults(final ILaunchConfigurationWorkingCopy configuration) {
         super.setDefaults(configuration);
     }
+
 }
